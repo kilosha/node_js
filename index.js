9d056003fd9475d6656c8fe16ee058fdfd350bbd
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2022-09-04 12:34:19 +0300

    switch to es6

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+import express from 'express';
+import bodyparser from 'body-parser';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyparser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyparser.json());
 app.listen(process.env.PORT, () => { console.log(`Now server is listening on port ${process.env.PORT}`)});
 
 const users = [
