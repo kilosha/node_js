55fcb6f9e9ad043cc59d94e074b27b556f4c53f8
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2022-09-04 12:29:15 +0300

    move port to env

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-app.listen(port, () => { console.log(`Now server is listening on port ${port}`)})
+app.use(bodyParser.json());
+app.listen(process.env.PORT, () => { console.log(`Now server is listening on port ${process.env.PORT}`)});
 
 const users = [
     {id: 1, name: "Pasha", isMan: true, age: 25},
