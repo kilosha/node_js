677a79fdef74db78bc8672cc02e4161d1c4b777b
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2022-09-14 13:24:44 +0300

    add sentry and swagger

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,16 +6,259 @@ const router = express.Router();
 
 router.get('/', UsersControllers.getUsers);
 
+/**
+ * @swagger
+ * /api/users/user/{ID}:
+ *  get:
+ *      summary: Get user by {ID}
+ *      description: Returns user object with ID
+ *      tags:
+ *          - Users
+ *      parameters:
+ *        - in: path
+ *          name: ID
+ *          required: true
+ *          description: Set an {ID} of a user to get
+ *          type: string
+ *      responses:
+ *           200:
+ *              description: Successful response
+ *              schema:
+ *                $ref: '#/definitions/CreatedOrUpdatedUser'
+ *           400:
+ *              description: Error
+ *              schema:
+ *                type: string
+ *                example: "Пользователь с ID 3c2af1c7-2bb9-4b33-b485-77f5a7d1d12a не найден"
+ */
 router.get('/user/:ID', Validator.validateID(), UsersControllers.getUserByID);
 
+/**
+ * @swagger
+ * /api/users/{param}:
+ *  get:
+ *      summary: Get filtered users by isMan or get user by ID
+ *      description: Returns users array or user with ID
+ *      tags:
+ *          - Users
+ *      parameters:
+ *        - in: path
+ *          name: param
+ *          required: true
+ *          description: If 'M' or 'F' get array of users with isMan true or false, if ID - return user with this ID
+ *          type: string
+ *      responses:
+ *           200:
+ *              description: Successful response
+ *              schema:
+ *                oneOf:
+ *                  - $ref: '#/definitions/UpdateUser'
+ *                  - $ref: '#/definitions/Users'
+ *           400:
+ *              description: Error
+ *              schema:
+ *                type: string
+ *                example: "Пользователь с ID 3c2af1c7-2bb9-4b33-b485-77f5a7d1d12a не найден"
+ */
+router.get('/:param', Validator.validateFilter(), UsersControllers.filterUsers);
+
+/**
+ * @swagger
+ *  /api/users:
+ *    post:
+ *      summary: Add new user and return new user object with ID if success
+ *      description:
+ *          Register 'User' object.
+ *      tags:
+ *          - Users
+ *      parameters:
+ *        - name: user
+ *          in: body
+ *          description: user object
+ *          required: true
+ *          schema:
+ *            $ref: '#/definitions/Users'
+ *      responses:
+ *        200:
+ *          description: Successful response
+ *          schema:
+ *            $ref: '#/definitions/CreatedOrUpdatedUser'
+ *        400:
+ *          description: Error
+ *          schema:
+ *            type: string
+ *            example: "Не заполнено обязательное поле username"
+ * definitions:
+ *   Users:
+ *     description: Users object
+ *     properties:
+ *       name:
+ *         type: string
+ *         example: Masha
+ *         description: Users' name
+ *       username:
+ *         type: string
+ *         example: marry22
+ *         description: Username
+ *       email:
+ *         type: string
+ *         example: example@example.com
+ *         description: Users' email 
+ *       isMan:
+ *         type: boolean
+ *         example: false
+ *         description: Man or woman
+ *       age:
+ *         type: integer
+ *         example: 25
+ *         description: Users' age
+ *     required:
+ *      - name
+ *      - username
+ *      - email
+ *      - isMan
+ *      - age
+ *   UpdateUser:
+ *     description: User with properties which should be updated
+ *     properties:
+ *       name:
+ *         type: string
+ *         example: Masha
+ *         description: Users' name
+ *       username:
+ *         type: string
+ *         example: marry22
+ *         description: Username
+ *       email:
+ *         type: string
+ *         example: example@example.com
+ *         description: Users' email 
+ *       isMan:
+ *         type: boolean
+ *         example: false
+ *         description: Man or woman
+ *       age:
+ *         type: integer
+ *         example: 25
+ *         description: Users' age
+ *   CreatedOrUpdatedUser:
+ *     description: Users object
+ *     properties:
+ *       ID:
+ *         type: string
+ *         example: d28e6dd0-a5ce-4c2c-8790-2ba0980007da
+ *       name:
+ *         type: string
+ *         example: Masha
+ *       username:
+ *         type: string
+ *         example: marry22
+ *       email:
+ *         type: string
+ *         example: example@example.com
+ *       isMan:
+ *         type: boolean
+ *         example: false
+ *       age:
+ *         type: integer
+ *         example: 25
+ */
 router.post("/", Validator.validateNewUser(), UsersControllers.createUser);
 
+/**
+ * @swagger
+ * /api/users/{ID}:
+ *  put:
+ *      summary: Updates a user with {ID}
+ *      tags:
+ *        - Users
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: path
+ *          name: ID
+ *          required: true
+ *          description: Set an {ID} of a user to update
+ *          type: string
+ *        - in: body
+ *          name: Users
+ *          required: true
+ *          description: Object to update
+ *          schema:
+ *              $ref: '#/definitions/UpdateUser'
+ *      responses:
+ *           200:
+ *              description: Successful response
+ *              schema:
+ *                $ref: '#/definitions/CreatedOrUpdatedUser'
+ *           400:
+ *              description: Error
+ *              schema:
+ *                type: string
+ *                example: "Укажите корректный email"
+ */
 router.put("/:ID", [Validator.validateID(), Validator.validateUser()], UsersControllers.updateFullUser);
 
+/**
+ * @swagger
+ * /api/users/{ID}:
+ *  patch:
+ *      summary: Updates a user with {ID}
+ *      tags:
+ *        - Users
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: path
+ *          name: ID
+ *          required: true
+ *          description: Set an {ID} of a user to update
+ *          type: string
+ *        - in: body
+ *          name: Users
+ *          required: true
+ *          description: Object to update
+ *          schema:
+ *              $ref: '#/definitions/UpdateUser'
+ *      responses:
+ *           200:
+ *              description: Successful response
+ *              schema:
+ *                $ref: '#/definitions/CreatedOrUpdatedUser'
+ *           400:
+ *              description: Error
+ *              schema:
+ *                type: string
+ *                example: "Укажите корректный email"
+ */
 router.patch('/:ID', [Validator.validateID(), Validator.validateUser()], UsersControllers.updateUser);
 
+/**
+ * @swagger
+ * /api/users/{ID}:
+ *  delete:
+ *      summary: Delete user with {ID}
+ *      tags:
+ *        - Users
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - in: path
+ *          name: ID
+ *          required: true
+ *          description: ID of user to delete
+ *          type: string
+ *      responses:
+ *           200:
+ *              description: Successful response
+ *              schema:
+ *                $ref: '#/definitions/CreatedOrUpdatedUser'
+ *           400:
+ *              description: Error
+ *              schema:
+ *                type: string
+ *                example: "Пользователь с ID 3c2af1c7-2bb9-4b33-b485-77f5a7d1d12a не найден"
+ */
 router.delete('/:ID', Validator.validateID(), UsersControllers.deleteUser);
-
-router.get('/:param', Validator.validateFilter(), UsersControllers.filterUsers);
 
 export default router;
