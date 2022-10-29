3ca930adde4c793565f96445b07c6d5f012819bc
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2022-10-29 23:54:20 +0300

    add ssl param to sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ import Sequelize from 'sequelize';
 let sequelize;
 
 if (process.env.NODE_ENV === 'production') {
-    sequelize = new Sequelize(process.env.DATABASE_URL);
+    sequelize = new Sequelize(process.env.DATABASE_URL, {ssl: { rejectUnauthorized: false }});
 } else {
     sequelize = new Sequelize('todo', 'postgres', 'admin', {
         host: 'localhost',
