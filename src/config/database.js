c4311a2bd757900477ebde4ca3962f4a9e6519e2
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2022-10-29 23:32:19 +0300

    add db

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,15 @@
+import e from 'express';
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('todo', 'postgres', 'admin', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+    sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+    sequelize = new Sequelize('todo', 'postgres', 'admin', {
+        host: 'localhost',
+        dialect: 'postgres'
+    });
+}
 
 export default sequelize;
